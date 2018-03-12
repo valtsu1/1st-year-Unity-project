2d1fbb64a8a761945f798e594206346553f59c32
ryhmapapu <34125586+ryhmapapu@users.noreply.github.com>
2018-03-12 09:48:09 +0200

    Add files via upload

---
+++ MusicStuff/FlyingMusicOff.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingMusicOff : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        FlyingMusicManager.Off();
+    }
+}

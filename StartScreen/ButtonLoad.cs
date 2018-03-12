bd1dab5c40303a68efb66db3b845311f43770065
ryhmapapu <34125586+ryhmapapu@users.noreply.github.com>
2018-03-12 09:50:37 +0200

    Add files via upload

---
+++ StartScreen/ButtonLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonLoad : MonoBehaviour {
+
+
+	private NextSceneManager nextScene;
+	private Button start;
+
+
+	void Start () {
+		start = GameObject.Find ("JeesusPerkele").GetComponent<Button> ();
+		nextScene = GameObject.Find ("NextSceneManager").GetComponent<NextSceneManager> ();
+
+		start.onClick.AddListener(NextSceneGo);
+	}
+	// Update is called once per frame
+	void NextSceneGo () {
+		nextScene.NextScene ();
+	
+	}
+}
